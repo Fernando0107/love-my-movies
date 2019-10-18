3b3c4d2dd2f61acf87dc4c81ad612388f8c11f9c
Fernando González <rmfernando@outlook.com>
2019-10-18 06:26:16 -0600

    templates, CSS, static folders were created. | Flask is also in app.py now.

--- app.py
+++ app.py
@@ -1,4 +1,13 @@
 import http.client
+from flask import Flask, render_template, jsonify, redirect
+import yaml
+import os
+
+developer = os.getenv("DEVELOPER", "User")
+environment = os.getenv("ENVIRONMENT", "development")
+
+app = Flask(__name__)
+
 
 conn = http.client.HTTPSConnection("api.themoviedb.org")
 
@@ -10,3 +19,13 @@ res = conn.getresponse()
 data = res.read()
 
 print(data.decode("utf-8"))
+
+@app.route('/')                                         #Es la ruta "home"
+def index():
+
+    return render_template("bio.html")
+
+
+if __name__ == '__main__':
+
+  app.run(host="0.0.0.0", debug=True)
